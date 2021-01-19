f6e98c234f49f3afd20fd55a82550b57f1979988
okastornov <okastornov@liveperson.com>
2021-01-19 19:51:12 +0200

    Removed redundant import

--- src/app/modules/index.ts
+++ src/app/modules/index.ts
@@ -1,9 +1,7 @@
-import { SharedModule } from './shared/shared.module';
 import { LandingModule } from './landing/landing.module';
 import { DashboardModule } from './dashboard/dashboard.module';
 
 export const modules =  [
-  SharedModule,
   LandingModule,
   DashboardModule
 ];
